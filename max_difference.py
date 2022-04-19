45efc418bdede64deb798008df573612ba48a02f
AhmedSayedMhvouz <ahmed.sayed.mhvouz.abdelhameed@gmail.com>
2022-04-19 14:26:23 +0200

    k

--- max_difference.py
+++ max_difference.py
@@ -8,15 +8,15 @@ def max_difference(a: list[int]) -> tuple[int, int]:
         small1, big1 = max_difference(first)
         small2, big2 = max_difference(second)
 
-        min_first = min(first)
+        minm_first = min(first)
         max_second = max(second)
 
-        if big2 - small2 > max_second - min_first and big2 - small2 > big1 - small1:
+        if big2 - small2 > max_second - minm_first and big2 - small2 > big1 - small1:
             return small2, big2
-        elif big1 - small1 > max_second - min_first:
+        elif big1 - small1 > max_second - minm_first:
             return small1, big1
         else:
-            return min_first, max_second
+            return minm_first, max_second
 
 
 if __name__ == "__main__":
